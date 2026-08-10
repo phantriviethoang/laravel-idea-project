42ec5e37996f42940e48f7daecb894f5048aad4c
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-08-10 19:19:59 +0700

    25-Tailwind Theme Setup And Initial UI

---
+++ resources/views/components/layout/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Idea</title>
+    @vite(['resources/css/app.css'])
+</head>
+
+<body class="bg-background text-foreground">
+    <x-layout.nav />
+
+    <main class="max-w-2xl mx-auto px-6 pb-10">
+        {{ $slot }}
+    </main>
+</body>
+</html>
