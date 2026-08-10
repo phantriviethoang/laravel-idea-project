ad5550a01a9d2fbb7cc98827fb4e16ddd2730b71
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-08-10 21:22:57 +0700

    Ep - 27 code viewer with CodeRabbit

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -33,9 +33,12 @@ class SessionsController extends Controller
         return redirect()->intended('/')->with('success', 'You are now logged in.');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
         Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
 
         return redirect('/');
     }
