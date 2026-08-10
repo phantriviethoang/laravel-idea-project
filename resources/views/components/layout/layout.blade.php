1e24d0b054aae8ea4fd6e5d4fe366a5d3a3957f8
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-08-10 21:17:34 +0700

    27-Flash Messaging and Interactivity with AlpineJS

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Idea</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-background text-foreground">
@@ -13,5 +13,10 @@
     <main class="max-w-2xl mx-auto px-6 pb-10">
         {{ $slot }}
     </main>
+
+    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
+        x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+        {{ $value }}
+    </div>
 </body>
 </html>
