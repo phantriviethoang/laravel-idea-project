bc41c08fd031c655a1c3aa99fe561ecf9a9c4aa1
PhanTriVietHoang <phantriviethoang@gmail.com>
2026-08-11 17:50:37 +0700

    28-Idea Cards

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -14,9 +14,11 @@
         {{ $slot }}
     </main>
 
+    @session('success')
     <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
         x-transition.opacity.duration.300ms class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
         {{ $value }}
     </div>
+    @endsession
 </body>
 </html>
